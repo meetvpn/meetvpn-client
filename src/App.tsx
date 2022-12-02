e15d7dc1102ec99421aa939d093ca4d456f18160
javier <javiernieto1989@gmail.com>
2022-12-02 11:07:48 -0500

    Added styles to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,37 @@
-import {
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-import {
-  IonApp,
-  IonRouterOutlet,
-  setupIonicReact
-} from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 
-import { IonReactRouter } from '@ionic/react-router';
+import { IonReactRouter } from "@ionic/react-router";
 
 import { AuthProvider } from "./AuthProvider";
 
-import MainTabs from './pages/MainTabs';
+import MainTabs from "./pages/MainTabs";
 import Login from "./pages/Login";
 
 import PublicRoute from "./components/PublicRoute";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+
+/* Custom variables and helpers class */
+import "./theme/helpers.css";
 
 setupIonicReact();
 
@@ -44,7 +40,12 @@ const App: React.FC = () => (
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet id="main">
-          <PublicRoute restricted={true} component={Login} path="/login" exact={true} />
+          <PublicRoute
+            restricted={true}
+            component={Login}
+            path="/login"
+            exact={true}
+          />
           <Route path="/tabs" render={() => <MainTabs />} />
           <Route exact path="/" render={() => <Redirect to="/tabs/home" />} />
         </IonRouterOutlet>
