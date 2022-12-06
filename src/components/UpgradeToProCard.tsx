3b1b2492e110c1fcf6043b30be7654f54a8b8234
javier <javiernieto1989@gmail.com>
2022-12-06 12:51:29 -0500

    Re-skinned UpgradeToProCard component

--- src/components/UpgradeToProCard.tsx
+++ src/components/UpgradeToProCard.tsx
@@ -1,20 +1,23 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import {IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon} from '@ionic/react';
+import BoldBigCrownIcon from '../assets/icon/bold-big-crown.svg';
 import './UpgradeToProCard.css';
 
 const UpgradeToProCard: React.FC = () => {
     return (
-        <IonCard>
+        <IonCard color="success" className="upgrade-card-container">
             <IonCardHeader>
-                <IonCardTitle>Upgrade to Pro</IonCardTitle>
+                <IonIcon src={BoldBigCrownIcon}></IonIcon>
+                <div>
+                    <IonCardTitle>Upgrade to Pro</IonCardTitle>
+                    <IonCardSubtitle>Access to all Servers. Dont worry you can cancel at any time</IonCardSubtitle>
+                </div>
             </IonCardHeader>
 
-            <IonCardContent>
-                Access to all Servers. Dont worry you can cancel at any time.
-            </IonCardContent>
-
-            <IonButton routerLink="/tabs/home" expand="block">Upgrade Now</IonButton>
+            <IonButton className="btn-upgrade-now"  expand="block">Upgrade Now</IonButton>
         </IonCard>
     );
 };
 
 export default UpgradeToProCard;
+
+// routerLink="/tabs/home"
